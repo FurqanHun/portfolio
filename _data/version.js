d7cff16b99884fa55e67d81866ed30066f418e29
0xQan <125211961+FurqanHun@users.noreply.github.com>
2026-07-01 11:33:45 +0000

    Remove obsolete THIRD_PARTY_LICENSES file and bump version iteration

--- _data/version.js
+++ _data/version.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 module.exports = function () {
   try {
     const currentDate = new Date().toISOString().split('T')[0];
-    const iteration = 15;
+    const iteration = 16;
 
     return `v3.${currentDate}.${iteration}`;
   } catch (error) {
